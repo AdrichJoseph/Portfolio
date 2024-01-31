0ec8a0635847970d62e1ef20d5a4abf2ad987aa4
AdrichJoseph <kippadrichjoseph@gmail.com>
2024-01-31 03:11:31 -0700

    data

---
+++ src/app/data.js
@@ -0,0 +1,118 @@
+// this comtains the data we are gonna for AboutMe component and slider component 
+
+export const AboutData = [
+  {
+    count: 1,
+    title: "UI:UX Designer",
+    img: "/UIUX.png",
+    array: [
+      "Balsamic Mockups",
+      "Wireframes",
+      "Prototyping",  
+      "Canva",
+      "Figma",
+    ],
+  },
+  {
+    count: 2,
+    title: "Front-end Developer",
+    img: "/frontEnd.png",
+    array: [
+      "HTML",
+      "javaScript",
+      "ReactJS",
+      "NextJS",
+      "TailwindCSS",
+    ],
+  },
+  {
+    count: 3,
+    title: "Back-end Developer",
+    img: "/backend.png",
+    array: [
+      "Python",
+      "Node.js",
+      "Kotlin",
+      "Flutter",
+      "C",
+      "C#",
+    ],
+  },
+  {
+    count: 4,
+    title: "Security",
+    img: "/security.png",
+    array: [
+      "Wireshark",
+      "Linux",
+      "VirtualBox",
+      "Cisco Packet Tracer",
+      "Encryption / Decryption",
+    ],
+  },
+  {
+    count: 5,
+    title: "Database",
+    img: "/database.png",
+    array: [
+      "MySQL",
+      "Database Design",
+      "Data optimization",
+    ],
+  },
+  {
+    count: 6,
+    title: "Tools and Things",
+    img: "/Tools.png",
+    array: [
+      "VSCode",
+      "Git / Github",
+      "Android Studio",
+      "Agile / Scrum",
+    ],
+  },
+];
+
+
+// this is for the slider  component where we show the projects
+
+export const DataArray = [
+  {
+    title: "Personal Portfolio Website Using Reactjs",
+    name: "My Porfolio Website",
+    des: "This project was built to showcase my skills and projects.",
+    des1: "This project was made using HTML, Reactjs, Nextjs, TailwindCSS,",
+    images: ["/Porfolio_Kipp.png"],
+  },
+  {
+    title: "Email Transfer Protocol",
+    name: "Secure email transfer protocol using encryption with python",
+    des: "Built a transfer protocol utilizing sockets for client-server messaging.",
+    des1: "Implemented RSA encryption for secure messaging and file transfer. Also employed SHA-1 hashing for secure file transfer.",
+
+    images: ["/EmailTransfer.png", "/Email2.png" ],
+  },
+  {
+    title: "Messaging and Collaborative App",
+    name: "Wasabi APP",
+    des: "This is my capstone project where users can collaborate and send messages, call and videocall.",
+    des1: "This is Created using flutter, MySQL and webrtc for video call and messaging.",
+    images: ["/Wasabi.png", "/Wasabi_2.png"],
+  },
+  {
+    title: "AI Chrome Extension for Language Translation",
+    name: "Tulai Chrome Extension",
+    des: "This app was built in HackED hackathon where the challenge was to help break barriers in society.",
+    des1: " This app was built using HTML, CSS, Python, and ChatGPT API.",
+
+    images: ["/Tulai_1.png", "/Tulai_2.png", "/Tulai_3.png"],
+  },
+  {
+    title: "A mobile app for managing employees",
+    name: "Android Employee Management App",
+    des: "We built this app as a excercise to learn how to deal with clients using AGILE methodology and scrums.",
+    des1: "This project was built using Kotlin, MySQL, and Android Studio.",
+
+    images: ["/Ritchie2.png", "/Ritchie.png", "/Ritchie4.png"],
+  },
+];
